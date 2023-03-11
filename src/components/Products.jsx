3c18e06147344e65f6f3051cefa51f84ba2f7947
Amirreza <amirreza.m13825@gmail.com>
2023-03-11 22:57:39 +0330

    add new product function added

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,42 @@
+import { useState } from "react";
+
 const ProductForm = ({ categories }) => {
+  const [productsFormData, setProductsFormData] = useState({
+    title: "",
+    quantity: "1",
+    categoryId: "",
+  });
+  const [products, setProducts] = useState([]);
+
+  // Handlers
+  const changeHandler = (e) => {
+    setProductsFormData({
+      ...productsFormData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const addNewProductHandler = (e) => {
+    e.preventDefault();
+
+    if (!productsFormData.title || !productsFormData.categoryId)
+      return alert("Please fill the form !");
+
+    const newProduct = {
+      ...productsFormData,
+      id: new Date().getTime(),
+      createdAt: new Date().toISOString(),
+    };
+
+    setProducts([...products, newProduct]);
+
+    setProductsFormData({
+      title: "",
+      quantity: "1",
+      categoryId: "",
+    });
+  };
+
   return (
     <div className="">
       <h2 className="text-xl text-slate-300 font-bold mb-2">Add New Product</h2>
@@ -8,8 +46,10 @@ const ProductForm = ({ categories }) => {
             Product Title
           </label>
           <input
+            value={productsFormData.title}
+            onChange={changeHandler}
             type="text"
-            name="product-title"
+            name="title"
             id="product-title"
             className="bg-transparent rounded border border-slate-500 text-slate-400"
           />
@@ -22,8 +62,10 @@ const ProductForm = ({ categories }) => {
             Quantity
           </label>
           <input
+            value={productsFormData.quantity}
+            onChange={changeHandler}
             type="number"
-            name="product-quantity"
+            name="quantity"
             id="product-quantity"
             className="bg-transparent rounded border border-slate-500 text-slate-400"
           />
@@ -36,7 +78,9 @@ const ProductForm = ({ categories }) => {
             Category
           </label>
           <select
-            name="product-category"
+            value={productsFormData.categoryId}
+            onChange={changeHandler}
+            name="categoryId"
             id="product-category"
             className="w-full bg-transparent rounded border border-slate-500 text-slate-400"
           >
@@ -58,6 +102,7 @@ const ProductForm = ({ categories }) => {
         </div>
         <div className="flex items-center justify-between gap-x-4">
           <button
+            onClick={addNewProductHandler}
             id="add-new-product"
             className="flex-1 bg-slate-500 text-slate-200 rounded-md py-2"
           >
