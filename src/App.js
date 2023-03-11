1821d6048a4a8c316e7bb8ade29e045a4e709be9
Amirreza <amirreza.m13825@gmail.com>
2023-03-11 19:11:50 +0330

    products and categories data added to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
 import "./App.css";
 
+const products = [
+  {
+    id: 1,
+    title: "Milk",
+    category: "Dairy",
+    createdAt: "2023-03-11T15:35:58.482Z",
+  },
+  {
+    id: 2,
+    title: "Cheese",
+    category: "Dairy",
+    createdAt: "2023-03-11T15:36:08.475Z",
+  },
+  {
+    id: 3,
+    title: "Butter",
+    category: "Dairy",
+    createdAt: "2023-03-11T15:36:18.601Z",
+  },
+];
+
+const categories = [
+  {
+    id: 1,
+    title: "Dairy",
+    createdAt: "2023-03-11T15:33:01.751Z",
+  },
+];
+
 function App() {
   return <div className="App"></div>;
 }
