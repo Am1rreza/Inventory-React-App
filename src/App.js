320c2b78eedef6dacbd90557238164d4a9dbbe05
Amirreza <amirreza.m13825@gmail.com>
2023-03-13 00:00:30 +0330

    save products and categories in local storage

--- src/App.js
+++ src/App.js
@@ -44,6 +44,26 @@ function App() {
     });
   };
 
+  useEffect(() => {
+    const savedProducts = JSON.parse(localStorage.getItem("products")) || [];
+    setProducts(savedProducts);
+    const savedCategories =
+      JSON.parse(localStorage.getItem("categories")) || [];
+    setCategories(savedCategories);
+  }, []);
+
+  useEffect(() => {
+    if (products.length) {
+      localStorage.setItem("products", JSON.stringify(products));
+    }
+  }, [products]);
+
+  useEffect(() => {
+    if (categories.length) {
+      localStorage.setItem("categories", JSON.stringify(categories));
+    }
+  }, [categories]);
+
   return (
     <div className="App">
       <div className="bg-slate-800 min-h-screen">
