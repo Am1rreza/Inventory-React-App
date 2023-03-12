e17013b6837ccec7a4492d75795fc056ad1be394
Amirreza <amirreza.m13825@gmail.com>
2023-03-12 22:25:49 +0330

    fix the responsive problem

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -25,38 +25,40 @@ const ProductsList = ({ setProducts, products, categories }) => {
   }
 
   return (
-    <>
+    <div>
       <h2 className="text-xl text-slate-300 font-bold mb-2">Product List</h2>
-      {products.map((product) => {
-        return (
-          <div
-            className="flex items-center justify-between mb-4"
-            key={product.id}
-          >
-            <span className="text-slate-400">{product.title}</span>
-            <div className="flex items-center gap-x-3">
-              <span className="text-slate-400">
-                {new Date(product.createdAt).toLocaleDateString("fa-IR")}
-              </span>
-              <span className="block px-3 py-0.5 text-slate-400 border border-slate-400 text-sm rounded-xl">
-                {getCategoryTitle(product.categoryId)}
-              </span>
+      <div className="overflow-x-auto">
+        {products.map((product) => {
+          return (
+            <div
+              className="flex items-center justify-between mb-4 w-full min-w-[400px]"
+              key={product.id}
+            >
+              <span className="text-slate-400">{product.title}</span>
+              <div className="flex items-center gap-x-3">
+                <span className="text-slate-400">
+                  {new Date(product.createdAt).toLocaleDateString("fa-IR")}
+                </span>
+                <span className="block px-3 py-0.5 text-slate-400 border border-slate-400 text-sm rounded-xl">
+                  {getCategoryTitle(product.categoryId)}
+                </span>
 
-              <span className="flex items-center justify-center w-7 h-7 rounded-full bg-slate-500 text-slate-300 border-2 border-slate-300">
-                {product.quantity}
-              </span>
-              <button
-                onClick={() => deleteProductHandler(product.id)}
-                className="delete-product border px-3 py-0.5 rounded-xl border-red-400 text-red-400 text-sm"
-              >
-                delete
-              </button>
+                <span className="flex items-center justify-center w-7 h-7 rounded-full bg-slate-500 text-slate-300 border-2 border-slate-300">
+                  {product.quantity}
+                </span>
+                <button
+                  onClick={() => deleteProductHandler(product.id)}
+                  className="delete-product border px-3 py-0.5 rounded-xl border-red-400 text-red-400 text-sm"
+                >
+                  delete
+                </button>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
       <br />
-    </>
+    </div>
   );
 };
 
